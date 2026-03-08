240372c7a6d2e70dd4335875ebe982ee5d7df8e1
FahriUyar <fahriuyar32@gmail.com>
2026-03-08 03:15:48 +0300

    fix: export defaultCategories constant

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,12 +1,95 @@
 export const defaultCategories = [
-  { name: "Barınma & Ev", type: "expense", subCategories: ["Aidat", "Kira"] },
-  { name: "Eğitim & Gelişim", type: "expense", subCategories: [] },
-  { name: "Eğlence & Sosyal Yaşam", type: "expense", subCategories: ["Dijital abonelik", "Hobiler & Oyun", "Sinema & Etkinlik", "Spor & Fitness", "Tatil & Konaklama"] },
-  { name: "Faturalar", type: "expense", subCategories: ["Doğalgaz", "Elektrik", "İnternet", "Su", "Telefon"] },
-  { name: "Finans & Ödemeler", type: "expense", subCategories: ["Kredi Kartı Ödemesi", "Kredi Taksiti", "Vergi & Ceza"] },
-  { name: "Giyim & Aksesuar", type: "expense", subCategories: ["Aksesuar & Çanta", "Ayakkabı", "Kıyafet", "Terzi & Kuru Temizleme"] },
-  { name: "Market & Mutfak", type: "expense", subCategories: ["Pazar", "Süpermarket"] },
-  { name: "Sağlık & Kişisel Bakım", type: "expense", subCategories: ["Doktor", "Eczane", "Kuaför/Berber"] },
-  { name: "Ulaşım", type: "expense", subCategories: ["Akaryakıt", "Araç bakımı", "Taksi", "Toplu taşıma"] },
-  { name: "Yeme & İçme (Dışarıda)", type: "expense", subCategories: ["Dışarıdan sipariş", "Restoran/Kafe"] }
+  {
+    name: "Barınma & Ev",
+    type: "expense",
+    subCategories: [
+      "Aidat",
+      "Ev Eşyası & Dekorasyon",
+      "Kira",
+      "Tadilat & Bakım",
+    ],
+  },
+  {
+    name: "Diğer",
+    type: "expense",
+    subCategories: ["Beklenmedik Giderler", "Hediyeler & Bağış"],
+  },
+  {
+    name: "Eğitim & Gelişim",
+    type: "expense",
+    subCategories: ["Kitap & Kırtasiye", "Kurs & Eğitim"],
+  },
+  {
+    name: "Eğlence & Sosyal Yaşam",
+    type: "expense",
+    subCategories: [
+      "Dijital Abonelik",
+      "Hobiler & Oyun",
+      "Sinema & Etkinlik",
+      "Spor & Fitness",
+      "Tatil & Konaklama",
+    ],
+  },
+  {
+    name: "Evcil Hayvan",
+    type: "expense",
+    subCategories: ["Mama & Kum", "Veteriner & Bakım"],
+  },
+  {
+    name: "Faturalar",
+    type: "expense",
+    subCategories: ["Doğalgaz", "Elektrik", "İnternet", "Su", "Telefon"],
+  },
+  {
+    name: "Finans & Ödemeler",
+    type: "expense",
+    subCategories: [
+      "Banka Kesintileri",
+      "Kredi Kartı Ödemesi",
+      "Kredi Taksiti",
+      "Sigorta & Kasko",
+      "Vergi & Ceza",
+    ],
+  },
+  {
+    name: "Giyim & Aksesuar",
+    type: "expense",
+    subCategories: [
+      "Aksesuar & Çanta",
+      "Ayakkabı",
+      "Kıyafet",
+      "Terzi & Kuru Temizleme",
+    ],
+  },
+  {
+    name: "Market & Mutfak",
+    type: "expense",
+    subCategories: ["Pazar", "Süpermarket"],
+  },
+  {
+    name: "Sağlık & Kişisel Bakım",
+    type: "expense",
+    subCategories: [
+      "Doktor",
+      "Eczane",
+      "Kozmetik & Kişisel Bakım",
+      "Kuaför & Berber",
+    ],
+  },
+  {
+    name: "Ulaşım",
+    type: "expense",
+    subCategories: [
+      "Akaryakıt",
+      "Araç Bakımı",
+      "Otopark & Geçiş Ücretleri",
+      "Taksi",
+      "Toplu Taşıma",
+    ],
+  },
+  {
+    name: "Yeme & İçme (Dışarıda)",
+    type: "expense",
+    subCategories: ["Dışarıdan Sipariş", "Restoran & Kafe"],
+  },
 ];
